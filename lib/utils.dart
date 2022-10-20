7476bf29d6e40dad5edbc4c211a365e53edc71f5
demola malomo <malomo.alaba@yahoo.com>
2022-10-21 00:09:22 +0100

    include logic

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,9 +1,9 @@
 class AppConstant {
-  final String databaseId = "6350190a8e7ca5486b97";
-  final String projectId = "63500c65be7c6bcc9d17";
-  final String collectionId = "635019149be78a3a4de9";
-  final String userId = "6350194507c3906d6e60";
-  final String endpoint = "http://192.168.1.8/v1";
+  final String databaseId = "DATABASE ID HERE";
+  final String projectId = "PROEJCT ID HERE";
+  final String collectionId = "COLLECTION ID";
+  final String userId = "USER ID";
+  final String endpoint = "ENDPOINT";
 }
 
 class User {
